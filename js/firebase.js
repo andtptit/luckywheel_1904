0a15f36d4d1fffa50e8a0c7f1effc784979c8884
An PC cong ty <andtptit@gmail.com>
2026-04-15 14:57:14 +0700

    Fix: Hardcoded firebase config as alternative to ENV for Vercel deployment

--- js/firebase.js
+++ js/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyD37syxua05lTj0GtkU1-YfSIY6VKyAoxA",
+  authDomain: "luckywheel1904.firebaseapp.com",
+  projectId: "luckywheel1904",
+  storageBucket: "luckywheel1904.firebasestorage.app",
+  messagingSenderId: "231784061850",
+  appId: "1:231784061850:web:26eca826a1c2b573f7b823",
+  measurementId: "G-SJF0RWXCET"
 };
 
 const app = initializeApp(firebaseConfig);
